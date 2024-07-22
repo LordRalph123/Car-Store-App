361b835b3cc107b2b9c0863f3e6bd7c9642d4950
LordRalph123 <raphaelseyindejoseph@gmail.com>
2024-07-22 23:24:25 +0100

    lite

--- lib/view/car_details.dart
+++ lib/view/car_details.dart
@@ -27,7 +27,7 @@ class _CarDetailsState extends State<CarDetails> {
           icon: Icon(
             Icons.arrow_back,
             size: 24.sp,
-            color: Color(0XFF849FFB),
+            color: Color(0XFF8E9FFB),
           ),
         ),
         actions: [
